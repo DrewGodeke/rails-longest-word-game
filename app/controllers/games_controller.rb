a92d12e8036bc8dd2e11787a3b0a505e35dbb98e
andrew godeke <legodeke@hotmail.com>
2024-04-06 16:46:10 -0400

    results

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,5 +4,20 @@ class GamesController < ApplicationController
   end
 
   def score
+    @word = params[:word]
+    @letters = params[:grid].split(',') # Set @letters from the grid parameter
+    valid_word = word_valid?(@letters, @word)
+
+    if valid_word
+      @result = "The word is a valid English word!"
+    else
+      @result = "The word cannot be built from the given grid."
+    end
+  end
+
+  private
+
+  def word_valid?(letters, word)
+    # Your validation logic here
   end
 end
